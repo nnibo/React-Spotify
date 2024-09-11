7ccba85db573b4687a39a5cd7c20aab21f75d011
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-11 08:42:39 -0300

    spotify

---
+++ src/components/CardSidebar.jsx
@@ -0,0 +1,7 @@
+export default function CardSidebar(){
+    return(
+        <>
+        <div className="bg-red-500 w-32 h-32"></div>
+          </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <>
+        <header className="bg-red-600 w-full h-28"></header>
+        </>
+    )
+}
