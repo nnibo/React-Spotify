205c68639672fde10052da5f3f9598a3c38fc699
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 08:26:29 -0300

    AJUDA NO BANCO

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import CardSidebar from "./components/CardSidebar"
 import Container from "./components/Container"
 import ConteudoPrincipal from "./components/ConteudoPrincipal"
@@ -5,6 +6,16 @@ import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
 
 function App() {
+
+  const[artistas, setArtistas] = useState([]);
+  const [isLoading, setIsLoading] = useState([false])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/artistas')
+    .then(res => res.json())
+    .then(data => setArtistas(data))
+    .catch(err => console.log(err))
+  }, [])
 
   return (
     <>
@@ -17,7 +28,19 @@ function App() {
           <CardSidebar/>
         </Sidebar>
 
-        <ConteudoPrincipal/>
+        <ConteudoPrincipal>
+        {
+          artistas.map( artista => (
+            <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <p>{artista.nome}</p>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+        </div>
+          ))
+        }
+        
+
+        </ConteudoPrincipal>
       </Container>
     </>
   )
